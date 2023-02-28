03ff9c9b9145143886d94374985d793b9a1fad05
“Indra <tristia71@gmail.com>
2023-02-28 15:02:25 +0700

    Add existing project files to Git

---
+++ src/components/Offline.js
@@ -0,0 +1,10 @@
+function Offline() {
+  return (
+    <>
+      <h1>Your Offline</h1>
+      <p>Sorry, but the page you were trying to view doesn't load.</p>
+    </>
+  )
+}
+
+export default Offline
